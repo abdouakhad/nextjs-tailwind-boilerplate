042adaea85e072ca1fa44170d1e5136d04d47a46
Abdoul Akhad Top <akhad0015@gmail.com>
2022-01-13 18:26:32 +0000

    follow readme file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 module.exports = {
-  content: [
+  purge: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './layout/**/*.{js,ts,jsx,tsx}',
+    './helpers/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      fontFamily: {
+        myFont: ['Inter'],
+      },
+      colors: {
+        beize: '#3E3E3E',
+        vert: '#34D399',
+        vertClair: '#10B981',
+        white: '#ffffff',
+        black: '#000000',
+        noir: '#111827',
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
